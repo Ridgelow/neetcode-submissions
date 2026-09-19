22f69e0991b29ce4c986129b7f2176503632dc63
Ridgelow <145386058+Ridgelow@users.noreply.github.com>
2026-09-18 21:54:57 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,17 @@
+
+variable = 23
+
+print(type(variable))
+
+variable = 23.2
+
+print(type(variable))
+
+variable = True
+
+
+print(type(variable))
+variable = 'hello'
+print(type(variable))
+variable = [1,2]
+print(type(variable))
