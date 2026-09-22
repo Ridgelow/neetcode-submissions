0cc56b4a28ebf808d5530c0ebbd2fc57e5dc9fd0
Ridgelow <145386058+Ridgelow@users.noreply.github.com>
2026-09-22 17:26:51 -0500

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,22 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+#1. show the dictionary
+print(your_dict)
+
+#2. print the value of a
+print(your_dict["a"])
+
+#3. print whether d is in the dictionary
+print("d" in your_dict)
+
+#4. reassign "a" to 4
+your_dict["a"] = 4
+
+#5. print the dictionary again
+print(your_dict)
+
+
